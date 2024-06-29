25537e22f6f98ec490f762616b59f366796aa1eb
murillolamego <murillolamegolopes@live.com>
2024-06-28 22:08:35 -0300

    initial navigation routes

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,12 +1,16 @@
+import { Outlet } from "react-router-dom";
 import CategoryItem from "../category-item/category-item.component";
 import "./directory.styles.scss";
 
 const Directory = ({ categories }) => {
   return (
-    <div className="directory-container">
-      {categories.map((category) => (
-        <CategoryItem category={category} key={category.id} />
-      ))}
+    <div>
+      <Outlet />
+      <div className="directory-container">
+        {categories.map((category) => (
+          <CategoryItem category={category} key={category.id} />
+        ))}
+      </div>
     </div>
   );
 };
